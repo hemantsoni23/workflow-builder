dda1d038a22960520594cb4b829205ad475e7ddb
Hemant soni <sonih5190@gmail.com>
2025-04-15 12:31:06 +0000

    ai directory navigation updated

--- nextjs-ui/src/app/page.js
+++ nextjs-ui/src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
   const handleLogin = () => setIsPortal(true);
 
   const handleNavigation = (path) => {
-    if (path === "/pipeline") {
+    if (path === "https://www.ai.noyco.com") {
       window.location.href = path;
       return;
     }
